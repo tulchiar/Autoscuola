443714a8816c1cc7f2c5b739d703c1b51d4e3f96
tulchiar <tulchiar@gmail.com>
2018-02-01 12:55:41 +0100

    build 12:55

--- src/it/tulchiar/autoscuola/Main.java
+++ src/it/tulchiar/autoscuola/Main.java
@@ -26,7 +26,7 @@ public class Main extends Application {
 			Model model = new Model();
 			controller.setModel(model);
 			
-			Scene scene = new Scene(root, 910, 768);
+			Scene scene = new Scene(root, 910, 680);
 
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
